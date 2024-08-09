e030cbc1596184f595a20effd67c510f23073e2d
Decapitated <brettzkyjo10@gmail.com>
2024-08-09 04:37:42 -0500

    Limit type for focused node and overlay.

--- rust/src/classes/focused_node.rs
+++ rust/src/classes/focused_node.rs
@@ -1,31 +1,19 @@
 use godot::prelude::*;
 use godot::builtin::NodePath;
-use godot::classes::{Resource, IResource};
+use godot::classes::Resource;
 
 #[derive(GodotClass)]
-#[class(tool, base=Resource)]
+#[class(tool, init, base=Resource)]
 pub struct FocusedNode {
     base: Base<Resource>,
     /// Node to focus on.
     #[export]
-    #[var(hint = NODE_TYPE, hint_string = "Control")]
+    #[var(hint = NODE_PATH_VALID_TYPES, hint_string = "Control")]
     pub target: NodePath,
     /// Overlay node for borders and other styles/effects.
     #[export]
-    #[var(hint = NODE_TYPE, hint_string = "Panel")]
+    #[var(hint = NODE_PATH_VALID_TYPES, hint_string = "Panel")]
     pub overlay: NodePath,
-}
-
-#[godot_api]
-impl IResource for FocusedNode {
-    fn init(base: Base<Resource>) -> Self {
-        // Set default values.
-        Self {
-            base,
-            target: NodePath::default(),
-            overlay: NodePath::default(),
-        }
-    }
 }
 
 #[godot_api]
