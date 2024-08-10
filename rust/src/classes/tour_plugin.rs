2f595d38a754f7804b966f89ede94d7889d6cdda
Decapitated <brettzkyjo10@gmail.com>
2024-08-10 00:36:37 -0500

    Capable of scripting tours from custom gdscript editor plugin.

--- rust/src/classes/tour_plugin.rs
+++ rust/src/classes/tour_plugin.rs
@@ -1,17 +1,12 @@
-use godot::classes::control::{CursorShape, LayoutPreset};
 use godot::prelude::*;
-use godot::classes::{EditorPlugin, IEditorPlugin, Button, Control, Panel};
+use godot::classes::{EditorPlugin, IEditorPlugin, Control};
 
-use super::focused_node::FocusedNode;
-use super::theater_rect::TheaterRect;
-
+use super::tour_singleton::TourSingleton;
 
 #[derive(GodotClass)]
 #[class(tool, editor_plugin, base=EditorPlugin)]
 pub struct TourPlugin {
     base: Base<EditorPlugin>,
-    theater_rect: Gd<TheaterRect>,
-    button: Gd<Button>,
 }
 
 #[godot_api]
@@ -19,8 +14,6 @@ impl IEditorPlugin for TourPlugin {
     fn init(base: Base<EditorPlugin>) -> Self {
         Self {
             base,
-            theater_rect: TheaterRect::new_alloc(),
-            button: Button::new_alloc(),
         }
     }
 
@@ -28,60 +21,27 @@ impl IEditorPlugin for TourPlugin {
         self.setup();
     }
 
-    fn exit_tree(&mut self) {
-        self.theater_rect.queue_free();
-        self.button.queue_free();
-    }
+    fn exit_tree(&mut self) {}
 }
 
 #[godot_api]
 impl TourPlugin {
     fn setup(&mut self) {
-        // Set TheaterRect defaults.
-        self.theater_rect.bind_mut().base_mut().set_anchors_preset(LayoutPreset::FULL_RECT);
-        self.theater_rect.bind_mut().base_mut().set_default_cursor_shape(CursorShape::FORBIDDEN);
-
-        let editor_interface_result = self.base_mut().get_editor_interface();
-        if let Some(editor_interface) = editor_interface_result {
-            if let Some(mut base_control) = editor_interface.get_base_control() {
-                let theater_rect_clone = self.theater_rect.clone();
-                base_control.add_child(theater_rect_clone);
-            }
+        if let Some(mut base_control) = self.get_base_control() {
+            let mut tour_singleton = TourPlugin::get_tour_singleton();
+            // Make plugin available to singleton.
+            tour_singleton.bind_mut().tour_plugin = Some(self.to_gd());
+            // Add TheaterRect to base_control.
+            base_control.add_child(tour_singleton.bind().theater_rect.clone());
         }
     }
 
-    #[func]
-    fn get_theater_rect(&mut self) -> Gd<TheaterRect> {
-        self.theater_rect.clone()
+    fn get_tour_singleton() -> Gd<TourSingleton> {
+        godot::classes::Engine::singleton().get_singleton(StringName::from("TourSingleton")).unwrap().cast::<TourSingleton>()
     }
 
     #[func]
-    fn create_focused_node(&mut self, target: Option<Gd<Control>>, overlay: Option<Gd<Panel>>) -> Gd<FocusedNode> {
-        let mut focused_node = FocusedNode::new_gd();
-        if let Some(target) = target {
-            focused_node.bind_mut().target = target.get_path();
-        }
-        if let Some(overlay) = overlay {
-            focused_node.bind_mut().overlay = overlay.get_path();
-        }
-        focused_node
-    }
-
-    #[func]
-    fn add_focused_node(&mut self, focused_node: Gd<FocusedNode>) {
-        let mut focused_nodes = self.theater_rect.bind().get_focused_nodes();
-        focused_nodes.push(Some(focused_node));
-    }
-
-    #[func]
-    fn remove_focused_node(&mut self, focused_node: Gd<FocusedNode>) {
-        let mut focused_nodes = self.theater_rect.bind().get_focused_nodes();
-        let value = Some(focused_node);
-        focused_nodes.erase(&value);
-    }
-
-    #[func]
-    fn get_base_control(&mut self) -> Option<Gd<Control>> {
+    pub fn get_base_control(&mut self) -> Option<Gd<Control>> {
         let editor_interface_result = self.base_mut().get_editor_interface();
         if let Some(editor_interface) = editor_interface_result {
             return editor_interface.get_base_control();
@@ -90,7 +50,7 @@ impl TourPlugin {
     }
 
     #[func]
-    fn get_title_bar(&mut self, base_control: Gd<Control>) -> Option<Gd<Control>> {
+    pub fn get_title_bar(&mut self, base_control: Gd<Control>) -> Option<Gd<Control>> {
         let title_bar_node = base_control.get_child(0).unwrap().get_child(0).unwrap();
         if let Ok(title_bar) = title_bar_node.try_cast::<Control>() {
             return Some(title_bar);
@@ -99,7 +59,7 @@ impl TourPlugin {
     }
 
     #[func]
-    fn get_title_bar_full(&mut self) -> Option<Gd<Control>> {
+    pub fn get_title_bar_full(&mut self) -> Option<Gd<Control>> {
         if let Some(base_control) = self.get_base_control() {
             return self.get_title_bar(base_control);
         }
@@ -107,7 +67,7 @@ impl TourPlugin {
     }
     
     #[func]
-    fn get_run_bar(&self, title_bar: Gd<Control>) -> Option<Gd<Control>> {
+    pub fn get_run_bar(&self, title_bar: Gd<Control>) -> Option<Gd<Control>> {
         if let Ok(run_bar) = title_bar.get_child(4).unwrap().try_cast::<Control>() {
             return Some(run_bar);
         }
@@ -115,7 +75,7 @@ impl TourPlugin {
     }
     
     #[func]
-    fn get_run_bar_full(&mut self) -> Option<Gd<Control>> {
+    pub fn get_run_bar_full(&mut self) -> Option<Gd<Control>> {
         if let Some(base_control) = self.get_base_control() {
             if let Some(title_bar) = self.get_title_bar(base_control) {
                 return self.get_run_bar(title_bar.clone());
---
+++ rust/src/classes/tour_singleton.rs
@@ -0,0 +1,104 @@
+use godot::classes::control::{CursorShape, LayoutPreset};
+use godot::prelude::*;
+use godot::classes::{Object, IObject, Control, Panel};
+
+use super::focused_node::FocusedNode;
+use super::theater_rect::TheaterRect;
+use super::tour_plugin::TourPlugin;
+
+#[derive(GodotClass)]
+#[class(base=Object)]
+pub struct TourSingleton {
+    base: Base<Object>,
+    #[export]
+    pub theater_rect: Gd<TheaterRect>,
+    pub tour_plugin: Option<Gd<TourPlugin>>,
+}
+
+#[godot_api]
+impl IObject for TourSingleton {
+    fn init(base: Base<Object>) -> Self {
+        let mut theater_rect = TheaterRect::new_alloc();
+        theater_rect.bind_mut().base_mut().set_anchors_preset(LayoutPreset::FULL_RECT);
+        theater_rect.bind_mut().base_mut().set_default_cursor_shape(CursorShape::FORBIDDEN);
+        Self {
+            base,
+            theater_rect,
+            tour_plugin: None
+        }
+    }
+}
+
+#[godot_api]
+impl TourSingleton {
+    #[func]
+    fn create_focused_node(&mut self, target: Option<Gd<Control>>, overlay: Option<Gd<Panel>>) -> Gd<FocusedNode> {
+        let mut focused_node = FocusedNode::new_gd();
+        if let Some(target) = target {
+            focused_node.bind_mut().target = target.get_path();
+        }
+        if let Some(overlay) = overlay {
+            focused_node.bind_mut().overlay = overlay.get_path();
+        }
+        focused_node
+    }
+
+    #[func]
+    fn add_focused_node(&mut self, focused_node: Gd<FocusedNode>) {
+        let mut focused_nodes = self.theater_rect.bind().get_focused_nodes();
+        focused_nodes.push(Some(focused_node));
+    }
+
+    #[func]
+    fn remove_focused_node(&mut self, focused_node: Gd<FocusedNode>) {
+        let mut focused_nodes = self.theater_rect.bind().get_focused_nodes();
+        let value = Some(focused_node);
+        focused_nodes.erase(&value);
+    }
+
+    #[func]
+    fn clear_focused_nodes(&mut self) {
+        let mut focused_nodes = self.theater_rect.bind().get_focused_nodes();
+        focused_nodes.clear();
+    }
+    
+    #[func]
+    pub fn get_base_control(&mut self) -> Option<Gd<Control>> {
+        if let Some(mut tour_plugin) = self.tour_plugin.clone() {
+            return tour_plugin.bind_mut().get_base_control();
+        }
+        None
+    }
+
+    #[func]
+    pub fn get_title_bar(&mut self, base_control: Gd<Control>) -> Option<Gd<Control>> {
+        if let Some(mut tour_plugin) = self.tour_plugin.clone() {
+            return tour_plugin.bind_mut().get_title_bar(base_control);
+        }
+        None
+    }
+
+    #[func]
+    pub fn get_title_bar_full(&mut self) -> Option<Gd<Control>> {
+        if let Some(mut tour_plugin) = self.tour_plugin.clone() {
+            return tour_plugin.bind_mut().get_title_bar_full();
+        }
+        None
+    }
+    
+    #[func]
+    pub fn get_run_bar(&self, title_bar: Gd<Control>) -> Option<Gd<Control>> {
+        if let Some(mut tour_plugin) = self.tour_plugin.clone() {
+            return tour_plugin.bind_mut().get_run_bar(title_bar);
+        }
+        None
+    }
+    
+    #[func]
+    pub fn get_run_bar_full(&mut self) -> Option<Gd<Control>> {
+        if let Some(mut tour_plugin) = self.tour_plugin.clone() {
+            return tour_plugin.bind_mut().get_run_bar_full();
+        }
+        None
+    }
+}
